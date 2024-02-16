c6283d1a42767701b6f0f9b212bf3359e2bb74c7
chirag7pandey <chirag7pandey@gmail.com>
2024-02-16 16:20:24 +0530

    bug fixed and completed

--- password_gen.js
+++ password_gen.js
@@ -17,15 +17,20 @@ let passwordLength=10;
 let checkCount=0;
 
 handleSlider();
+//inidicator color set to gray
+setIndicator("#ccc");
 
 //set password length
 function handleSlider(){
     inputSlider.value=passwordLength;
     lengthDisplay.innerText=passwordLength;
+    const min=inputSlider.min;
+    const max=inputSlider.max;
+    inputSlider.style.backgroundSize=((passwordLength-min)*100/(max-min) + "% 100%");
 }
 function setIndicator(color){
     indicator.style.backgroundColor =color;
-    indicator.style.boxShadow="10px 20px 30px gray";
+    indicator.style.boxShadow=`0px 0px 20px ${color}`;
 }
 
 function getRndInteger(min,max){
@@ -83,10 +88,12 @@ async function copyContent(){
     catch(e){
         dataCopyMsg.innerText="failed";
     }
-    //make span visible
-    copyMsg.classList.add("active");
+
+    //make span visible adding active class
+    dataCopyMsg.classList.add("active");
+    //remove after 2s ehrn copy done 
     setTimeout(() => {
-        copyMsg.classList.remove("active");
+        dataCopyMsg.classList.remove("active");
     },2000);
 }
 
